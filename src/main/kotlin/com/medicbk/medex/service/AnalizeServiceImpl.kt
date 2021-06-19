4beede6aab6fcf673c01ceb67f65d8fca1aee1a9
maxim_d <mdondukov@protonmail.ch>
2021-06-20 01:24:15 +0600

    Update AnalizeServiceImpl.kt

--- src/main/kotlin/com/medicbk/medex/service/AnalizeServiceImpl.kt
+++ src/main/kotlin/com/medicbk/medex/service/AnalizeServiceImpl.kt
@@ -1,25 +1,20 @@
 package com.medicbk.medex.service
 
-import com.medicbk.medex.model.Document
 import com.medicbk.medex.service.handler.DataHandler
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.time.LocalDate
-import java.time.LocalTime
 
 @Service
 class AnalizeServiceImpl : AnalizeService {
 
     @Autowired
+    private lateinit var documentService: DocumentService
+
+    @Autowired
     private lateinit var handlers: List<DataHandler>
 
     override fun analize(content: String): List<Analize> {
-        val document = Document(
-            date = LocalDate.now(),
-            time = LocalTime.now(),
-            diagnosis = ""
-        )
-
+        val document = documentService.getDocument(content)
         val analizes = mutableListOf<Analize>()
         handlers.forEach { handler -> handler.handle(document).also { analizes.add(it) } }
         return analizes
