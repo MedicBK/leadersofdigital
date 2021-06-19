568629db808fe9a279890e7df6bb8f79bb0c81e3
maxim_d <mdondukov@protonmail.ch>
2021-06-19 19:25:14 +0600

    Add ref repositories

---
+++ src/main/kotlin/com/medicbk/medex/model/Document.kt
@@ -0,0 +1,30 @@
+package com.medicbk.medex.model
+
+import java.time.LocalDateTime
+
+data class Document(
+    val name: String,
+    val dt: LocalDateTime,
+    val diagnosis: DiagnosisField,
+    val exams: List<Field> = emptyList(),
+    val analyses: List<AnalysisField> = emptyList(),
+    val advices: List<Field> = emptyList()
+) {
+    data class Field(
+        val id: Long,
+        val name: String,
+        val dt: LocalDateTime
+    )
+
+    data class DiagnosisField(
+        val name: String,
+        val mkb: String
+    )
+
+    data class AnalysisField(
+        val id: Long,
+        val name: String,
+        val dt: LocalDateTime,
+        val result: String
+    )
+}
