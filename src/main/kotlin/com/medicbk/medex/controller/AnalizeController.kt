774037f484cdb49ba910b7bcc2eefa8de17b1be9
maxim_d <mdondukov@protonmail.ch>
2021-06-19 23:53:33 +0600

    Update AnalizeController.kt

--- src/main/kotlin/com/medicbk/medex/controller/AnalizeController.kt
+++ src/main/kotlin/com/medicbk/medex/controller/AnalizeController.kt
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.medicbk.medex.service.parser.service.AnalizeService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["*"])
 class AnalizeController {
 
     @Autowired
