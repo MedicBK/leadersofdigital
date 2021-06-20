6c105a0e620e684a3b4cad39c8d43d60bbde6f46
maxim_d <mdondukov@protonmail.ch>
2021-06-20 09:33:20 +0600

    Little fix

--- src/main/kotlin/com/medicbk/medex/service/handler/DrugsHandler.kt
+++ src/main/kotlin/com/medicbk/medex/service/handler/DrugsHandler.kt
@@ -11,18 +11,26 @@ import org.springframework.stereotype.Component
 @Order(3)
 class DrugsHandler : DataHandler {
 
+    @Value("\${data.nosology.drugs}")
+    private val nosologyDrugs: List<String> = emptyList()
+
     @Value("\${data.section.header.drugs}")
     private lateinit var header: String
 
     override fun handle(document: Document): Analize {
         val drugs = mutableListOf<Analize.Item>()
         document.treatment[TreatmentType.MEDICATION]?.forEach { treat ->
-            drugs.add(Analize.Item(text = "${treat.name} ${treat.description}", false))
+            drugs.add(
+                Analize.Item(
+                    text = "${treat.name} ${treat.description}",
+                    nosologyDrugs.map { it.lowercase().trim() }.contains(treat.name.lowercase().trim())
+                )
+            )
         }
 
         return Analize(
             title = header,
-            items = drugs,
+            items = drugs.sortedByDescending { it.checked },
             type = Analize.Type.DRUGS
         )
     }
